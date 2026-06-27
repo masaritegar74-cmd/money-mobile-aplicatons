69e4124f7d5a635b4ea173b2b8c012b168e6f621
Vibesdk <vibesdk-bot@cloudflare.com>
2026-06-27 07:30:12 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add @react-native-async-storage/async-storage"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
